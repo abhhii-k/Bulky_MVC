df28ed62520ccb65229ac9c0ba21fdfd6af61220
abhhii-k <kabhik2898@gmail.com>
2023-08-14 04:31:56 +0530

    Category Create operation completed.

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,15 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, DispayOrder = 1, Name = "Action" },
+                new Category { id = 2, DispayOrder = 2, Name = "Sci-Fi" },
+                new Category { id = 3, DispayOrder = 3, Name = "Rom-Com" },
+                new Category { id = 4, DispayOrder = 4, Name = "Horror" }
+                );
+        }
     }
 }
